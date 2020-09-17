a22497c396fcd871a0ad01cfb3c477acb2707904
dekunle <dekunle02@gmail.com>
2020-09-17 23:46:48 +0100

    Initial Commit

---
+++ app/src/main/java/com/adeleke/samad/birthdayreminder/network/FirebaseUtil.kt
@@ -0,0 +1,13 @@
+package com.adeleke.samad.birthdayreminder.network
+
+import com.google.firebase.auth.FirebaseAuth
+
+class FirebaseUtil {
+
+    private var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun getCurrentUser() {
+
+    }
+
+}
